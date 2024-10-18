6c76da0fa68619383472167e70be2f7d927fa814
yoonhyeonjeong <h_j111@naver.com>
2024-10-18 16:20:49 +0900

    custom hook

--- useref/src/App.jsx
+++ useref/src/App.jsx
@@ -1,15 +1,51 @@
 import "./App.css";
-import {useState, useRef} from "react";
+import {useState, useRef, useEffect} from "react";
 
 function App() {
+    const {count, increment, decrement} = useCounter(0, 5);
+    // 고양이 사진 오픈 API 주소
+    const catUrl = "https://api.thecatapi.com/v1/images/search";
+    // const [fetchUrl, setFetchUrl] = useState(catUrl); // 상태로 url관리
+    const {loading, data, fetchData} = useFetch(catUrl);
     return (
         <>
-            <ControlledInput />
-            <UseRefInput />
-            <Counter />
+            <div>counter: {count}</div>
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
+            <button
+                onClick={() => {
+                    fetchData(catUrl);
+                }}
+            >
+                고양이 데이터 가져오기
+            </button>
+            {loading ? <p>loading중</p> : data && <img src={data[0].url}></img>}
         </>
     );
 }
+const useFetch = url => {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const fetchData = () => {
+        setLoading(true);
+        fetch(url)
+            .then(res => res.json())
+            .then(res => {
+                setData(res);
+                setLoading(false);
+            })
+            .catch(err => {
+                setError(err);
+                setLoading(false);
+            });
+    };
+    useEffect(() => {
+        fetchData();
+    }, [url]);
+    return {loading, data, fetchData};
+};
+
 const Counter = () => {
     const [counter, setCounter] = useState(0);
     const numberRef = useRef(null);
@@ -56,5 +92,12 @@ const ControlledInput = () => {
         />
     );
 };
+// custom Hook
+const useCounter = (initialValue = 0, step = 1) => {
+    const [count, setCount] = useState(initialValue, step);
+    const increment = () => setCount(prev => prev + step);
+    const decrement = () => setCount(prev => prev - step);
 
+    return {count, increment, decrement};
+};
 export default App;
