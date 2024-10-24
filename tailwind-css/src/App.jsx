ef22a1f57d9ded6b3775834b72b7dd4e1125b83a
yoonhyeonjeong <h_j111@naver.com>
2024-10-24 14:08:55 +0900

    tailwind-css

---
+++ tailwind-css/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div className="text-2xl">개피건</div>
+            <div className="flex">
+                <div className="flex-none w-14">01</div>
+                <div className="flex-initial w-64">02</div>
+                <div className="flex-initial w-32">03</div>
+            </div>
+        </>
+    );
+}
+
+export default App;
